60f38f478f0e80841c3dc2ca815b1fdb0a05ea4a
unknown <mohmmedtouhid007@gmail.com>
2024-04-10 17:58:20 +0600

    Socket url updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chat-app-hedr.onrender.com", {
         query: {
           userId: authUser._id,
         },
