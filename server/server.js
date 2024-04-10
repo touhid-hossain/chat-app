7cebea72bcd36848e788042c38d9ccb405c3b00a
unknown <mohmmedtouhid007@gmail.com>
2024-04-10 17:42:16 +0600

    Deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import env from "dotenv";
 import authRoutes from "./routers/auth.route.js";
 import messageRoutes from "./routers/message.route.js";
@@ -6,6 +7,8 @@ import userRoutes from "./routers/user.route.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./connection/connectionDB.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 env.config();
 const PORT = process.env.PORT || 6000;
@@ -20,6 +23,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// this function is running the frontend and backend in the same port
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
